17f30aa4fcaa31625afb53500e4eb138c14a8d1f
Alexander Jarvi <jarvi.alex@gmail.com>
2024-08-29 21:37:32 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import Header from './Header'
+import logo from './logo.svg';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      
+      <header className="App-header">
         <Header></Header>
-        <main>
-    
-        </main>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-
 
 export default App;
